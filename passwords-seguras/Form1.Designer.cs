47715b4a5dea0e3d1fad35f947fc321329839725
karolinaconk <karolina14badilla@gmail.com>
2023-05-04 23:31:17 -0700

    validar instantaneamente

--- passwords-seguras/Form1.Designer.cs
+++ passwords-seguras/Form1.Designer.cs
@@ -39,12 +39,24 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label_username = new System.Windows.Forms.Label();
+            this.label_email = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.label_confirm_password = new System.Windows.Forms.Label();
+            this.error_password = new System.Windows.Forms.PictureBox();
+            this.error_confirm_password = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.error_email)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.error_username)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.error_password)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.error_confirm_password)).BeginInit();
             this.SuspendLayout();
             // 
             // text_username
@@ -56,7 +68,6 @@
             this.text_username.Name = "text_username";
             this.text_username.Size = new System.Drawing.Size(359, 29);
             this.text_username.TabIndex = 0;
-            this.text_username.Text = "username";
             this.text_username.TextChanged += new System.EventHandler(this.text_username_TextChanged);
             // 
             // text_email
@@ -68,7 +79,6 @@
             this.text_email.Name = "text_email";
             this.text_email.Size = new System.Drawing.Size(359, 29);
             this.text_email.TabIndex = 1;
-            this.text_email.Text = "email";
             this.text_email.TextChanged += new System.EventHandler(this.text_email_TextChanged);
             // 
             // text_confirm_password
@@ -78,9 +88,9 @@
             this.text_confirm_password.Location = new System.Drawing.Point(233, 283);
             this.text_confirm_password.Multiline = true;
             this.text_confirm_password.Name = "text_confirm_password";
+            this.text_confirm_password.PasswordChar = '*';
             this.text_confirm_password.Size = new System.Drawing.Size(359, 29);
             this.text_confirm_password.TabIndex = 3;
-            this.text_confirm_password.Text = "confirm password";
             this.text_confirm_password.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // text_password
@@ -90,9 +100,10 @@
             this.text_password.Location = new System.Drawing.Point(233, 231);
             this.text_password.Multiline = true;
             this.text_password.Name = "text_password";
+            this.text_password.PasswordChar = '*';
             this.text_password.Size = new System.Drawing.Size(359, 29);
             this.text_password.TabIndex = 2;
-            this.text_password.Text = "password";
+            this.text_password.TextChanged += new System.EventHandler(this.text_password_TextChanged);
             // 
             // button1
             // 
@@ -163,7 +174,104 @@
             this.pictureBox1.Size = new System.Drawing.Size(32, 29);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label_username
+            // 
+            this.label_username.AutoSize = true;
+            this.label_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_username.ForeColor = System.Drawing.Color.DarkRed;
+            this.label_username.Location = new System.Drawing.Point(233, 110);
+            this.label_username.Name = "label_username";
+            this.label_username.Size = new System.Drawing.Size(11, 15);
+            this.label_username.TabIndex = 11;
+            this.label_username.Text = " ";
+            // 
+            // label_email
+            // 
+            this.label_email.AutoSize = true;
+            this.label_email.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_email.ForeColor = System.Drawing.Color.DarkRed;
+            this.label_email.Location = new System.Drawing.Point(233, 163);
+            this.label_email.Name = "label_email";
+            this.label_email.Size = new System.Drawing.Size(11, 15);
+            this.label_email.TabIndex = 12;
+            this.label_email.Text = " ";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_password.ForeColor = System.Drawing.Color.DarkRed;
+            this.label_password.Location = new System.Drawing.Point(233, 213);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(11, 15);
+            this.label_password.TabIndex = 13;
+            this.label_password.Text = " ";
+            // 
+            // label_confirm_password
+            // 
+            this.label_confirm_password.AutoSize = true;
+            this.label_confirm_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_confirm_password.ForeColor = System.Drawing.Color.DarkRed;
+            this.label_confirm_password.Location = new System.Drawing.Point(233, 263);
+            this.label_confirm_password.Name = "label_confirm_password";
+            this.label_confirm_password.Size = new System.Drawing.Size(11, 15);
+            this.label_confirm_password.TabIndex = 14;
+            this.label_confirm_password.Text = " ";
+            // 
+            // error_password
+            // 
+            this.error_password.Location = new System.Drawing.Point(598, 231);
+            this.error_password.Name = "error_password";
+            this.error_password.Size = new System.Drawing.Size(34, 29);
+            this.error_password.TabIndex = 15;
+            this.error_password.TabStop = false;
+            // 
+            // error_confirm_password
+            // 
+            this.error_confirm_password.Location = new System.Drawing.Point(598, 283);
+            this.error_confirm_password.Name = "error_confirm_password";
+            this.error_confirm_password.Size = new System.Drawing.Size(34, 29);
+            this.error_confirm_password.TabIndex = 16;
+            this.error_confirm_password.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LimeGreen;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(189, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 31);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Register";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(313, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 31);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Account";
             // 
             // Form1
             // 
@@ -171,6 +279,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(804, 461);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.error_confirm_password);
+            this.Controls.Add(this.error_password);
+            this.Controls.Add(this.label_confirm_password);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_email);
+            this.Controls.Add(this.label_username);
             this.Controls.Add(this.error_email);
             this.Controls.Add(this.error_username);
             this.Controls.Add(this.button1);
@@ -183,8 +301,6 @@
             this.Controls.Add(this.text_email);
             this.Controls.Add(this.text_username);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -195,6 +311,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.error_password)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.error_confirm_password)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -213,6 +331,16 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox error_username;
         private System.Windows.Forms.PictureBox error_email;
+        private System.Windows.Forms.Label label_username;
+        private System.Windows.Forms.Label label_email;
+        private System.Windows.Forms.Label label_password;
+        private System.Windows.Forms.Label label_confirm_password;
+        private System.Windows.Forms.PictureBox error_password;
+        private System.Windows.Forms.PictureBox error_confirm_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- passwords-seguras/Form1.cs
+++ passwords-seguras/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
@@ -13,46 +15,108 @@ namespace passwords_seguras
 {
     public partial class Form1 : Form
     {
+        private bool valid_username = false;
+        private bool valid_email = false;
+        private bool valid_password = false;
+        private bool valid_confirm_password = false;
         public Form1()
         {
             InitializeComponent();
+            text_email.Text = "Ingresa tu email aquí";
+            text_email.ForeColor = Color.Gray;
+
+            text_email.Enter += text_email_Enter;
+            text_email.Leave += text_email_Leave;
+
+            text_username.Text = "Ingresa tu username aquí";
+            text_username.ForeColor = Color.Gray;
+
+            text_username.Enter += text_username_Enter;
+            text_username.Leave += text_username_Leave;
+            button1.Enabled = false;
+
+  
+            /**
+            else
+            {
+                button1.Enabled = true;
+            }
+            
+            else if (valid_username == true && valid_password == true && valid_email == true && valid_confirm_password == true)
+            {
+                button1.Enabled = true;
+            }
+            **/
+        }
+        private void text_username_Enter(object sender, EventArgs e)
+        {
+            if (text_username.Text == "Ingresa tu username aquí")
+            {
+                text_username.Text = "";
+                text_username.ForeColor = Color.Black;
+            }
+        }
+
+        private void text_username_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(text_username.Text))
+            {
+                text_username.Text = "Ingresa tu username aquí";
+                text_username.ForeColor = Color.Gray;
+            }
+        }
+
+        private void text_email_Enter(object sender, EventArgs e)
+        {
+            if (text_email.Text == "Ingresa tu email aquí")
+            {
+                text_email.Text = "";
+                text_email.ForeColor = Color.Black;
+            }
+        }
+
+        private void text_email_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(text_email.Text))
+            {
+                text_email.Text = "Ingresa tu email aquí";
+                text_email.ForeColor = Color.Gray;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
+            string password = text_password.Text;
 
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string valor_username = text_username.Text;
-            string valor_email = text_email.Text;
-            string valor_password = text_password.Text;
-            string valor_confirm_password = text_confirm_password.Text;
-
-            if (valor_password != valor_confirm_password)
+            if (text_confirm_password.Text == password)
             {
-                MessageBox.Show("Las contraseñas no coinciden intenta de nuevo");
+                error_confirm_password.Image = Properties.Resources.checkbox_fill; // Cambia el icono a verde
+                error_confirm_password.Visible = true; // Oculta el icono
+                label_confirm_password.Text = " ";
+                valid_confirm_password = true;
             }
             else
             {
-                if (valor_password.Length >= 12)
-                {
-                    MessageBox.Show("Bienvenido :)");
-                }
+                label_confirm_password.Text = "Las passwords no coinciden";
+                error_confirm_password.Image = Properties.Resources.error_warning_fill; // Cambia el icono a rojo
+                error_confirm_password.Visible = true; // Muestra el icono
+                valid_confirm_password = false;
+            }
 
+            if (valid_username == true && valid_password == true && valid_email == true && valid_confirm_password == true)
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
             }
         }
+
 
         private void text_username_TextChanged(object sender, EventArgs e)
         {
@@ -68,15 +132,26 @@ namespace passwords_seguras
                 }
             }
 
-            if (hasInvalidChars || (text_username.Text.Length > 0 && char.IsDigit(text_username.Text[0])) || text_username.Text.Length < 12)
+            if (hasInvalidChars || (text_username.Text.Length > 0 && char.IsDigit(text_username.Text[0])) || text_username.Text.Length < 3)
             {
                 error_username.Image = Properties.Resources.error_warning_fill; // Cambia el icono a rojo
                 error_username.Visible = true; // Muestra el icono
+                valid_username = false;
+                if (hasInvalidChars)
+                {
+                    label_username.Text = "No se permiten caracteres especiales";
+                } 
+                else if ((text_username.Text.Length > 0 && char.IsDigit(text_username.Text[0])))
+                {
+                    label_username.Text = "No se permite el primer dígito sea número";
+                }
             }
             else
             {
+                label_username.Text = " ";
                 error_username.Image = Properties.Resources.checkbox_fill; // Cambia el icono a verde
                 error_username.Visible = true; // Oculta el icono
+                valid_username = true;
             }
         }
         
@@ -93,18 +168,60 @@ namespace passwords_seguras
                     break;
                 }
             }
+
             int indiceArroba = text_email.Text.IndexOf('@');
             int indicePunto = text_email.Text.IndexOf('.');
             if (hasInvalidChars || (text_email.Text.Length > 0 && char.IsDigit(text_email.Text[0]) && text_email.Text[0] == '@' && text_email.Text[0] == '.') || !text_email.Text.Contains("@") || text_email.Text.Length < 6 || indiceArroba > indicePunto)
             {
                 error_email.Image = Properties.Resources.error_warning_fill; // Cambia el icono a rojo
                 error_email.Visible = true; // Muestra el icono
+                valid_email = false;
+                if (hasInvalidChars)
+                {
+                    label_email.Text = "No se permiten caracteres especiales";
+                }
+                else if ((text_email.Text.Length > 0 && char.IsDigit(text_email.Text[0])))
+                {
+                    label_email.Text = "No se permite el primer dígito sea número";
+                }
+                else if (indiceArroba > indicePunto) {
+                    label_email.Text = "No es correcto este formato de email";
+                }
             }
             else
             {
+                label_email.Text = " ";
                 error_email.Image = Properties.Resources.checkbox_fill; // Cambia el icono a verde
                 error_email.Visible = true; // Oculta el icono
+                valid_email = true;
             }
+        }
+
+        private void text_password_TextChanged(object sender, EventArgs e)
+        {
+            string password = text_password.Text;
+            bool isValid = Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*[@$!%*?&])(?!.*\s).{12,}$");
+
+            
+            if (isValid)
+            {
+                error_password.Image = Properties.Resources.checkbox_fill; // Cambia el icono a verde
+                error_password.Visible = true; // Oculta el icono
+                label_password.Text = " ";
+                valid_password = true;
+            }
+            else
+            {
+                label_password.Text = "Por lo menos una mayúscula, un número, un símbolo y 12 caracteres";
+                error_password.Image = Properties.Resources.error_warning_fill; // Cambia el icono a rojo
+                error_password.Visible = true; // Muestra el icono
+                valid_password = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido :)" + valid_username + valid_email + valid_password + valid_confirm_password);
         }
     }
 }
